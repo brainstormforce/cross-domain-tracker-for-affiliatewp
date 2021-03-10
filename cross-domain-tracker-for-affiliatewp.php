39454e295d94d270a88ba7f00e3defa04491cf21
hemant7405 <hdkothari81@gmail.com>
2021-03-10 15:14:02 +0530

    tested and updated the Woocommerce latest stable version

--- cross-domain-tracker-for-affiliatewp.php
+++ cross-domain-tracker-for-affiliatewp.php
@@ -9,7 +9,7 @@
  * Text Domain: affiliatewp-external-visits
  * Domain Path: languages
  * WC requires at least: 3.0
- * WC tested up to: 3.5.1
+ * WC tested up to: 5.1.0
  * PHP version 5
  *
  * @category PHP
